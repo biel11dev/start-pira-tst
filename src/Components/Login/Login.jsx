ce3948aac07b0fffb71487f9478bb4b9c3425d04
Gabriel Jorge Bezerra Ribeiro <gabriel.ribeiro@sppserv.com.br>
2025-02-19 13:28:24 -0300

    teste não aguento mais

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { FaLock, FaUser } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Login.css";
 
 const Login = ({ setIsAuthenticated }) => {
